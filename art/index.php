f81c542d85e0257e6388d0fdf29ec2c41d865734
root <you@example.com>
2017-07-20 14:44:01 +0300

    first commit

---
+++ art/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet"   type="text/css" href="css/style.css">
+	<title>Home</title>
+</head>
+<body>
+  
+  <div class="wrapper">
+
+<!--HEADER MENU-->
+
+<div class="header">
+     <div class="header-menu">
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">About</a></li>
+		<li><a href="#">Projects</a></li>
+        <li><a href="#">Festi</a></li> 	
+	</ul>
+    </div>
+</div>
+
+<!--END OF HEADER MENU-->
+
+<!--CONTENT FOR HOME PAGE-->
+
+<div class="conteiner row">
+	
+
+    <div id="content">
+    <div class="in-content">
+    	
+    
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum ipsa asperiores voluptatibus repellat, pariatur error distinctio. Rerum asperiores impedit alias, quas, distinctio doloremque, quasi facilis soluta repellendus deleniti eius dicta.
+
+     </div>
+    </div>
+     
+
+        <div id="sid">
+
+         <div class="in-sidebar"><h2>SideBar</h2>
+
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, voluptas at, est numquam nisi maiores voluptates similique earum ea facere doloremque rerum accusamus quia fugiat nostrum eum sunt. Fugiat, ullam!
+
+         </div> 
+
+
+         </div>
+</div>
+
+
+    <div class="footer">
+	Footer
+    </div>
+
+</div>
+ 
+</body>
+</html>
